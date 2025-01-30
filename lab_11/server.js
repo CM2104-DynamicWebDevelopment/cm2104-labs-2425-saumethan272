8cfae225962c64117f86d7399b4060895d101b60
saumethan272 <e.saum@rgu.ac.uk>
2025-01-30 10:27:54 +0000

    added jokes

--- lab_11/server.js
+++ lab_11/server.js
@@ -9,4 +9,12 @@ app.get("/test", function(req, res){
     res.send("this is route 2");
 });
 
+app.get("/joke", function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var randomJoke = knockknock()
+    res.end(randomJoke);
+
+})
+
 app.listen(8080);
