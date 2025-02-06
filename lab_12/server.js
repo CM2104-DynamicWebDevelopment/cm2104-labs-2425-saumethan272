3a31ad3e9f4d717cf5330e5dae38974e0d2ef882
saumethan272 <e.saum@rgu.ac.uk>
2025-02-06 11:00:28 +0000

    lab12

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
 var SpotifyWebApi = require("spotify-web-api-node");
+
 app.use(express.static("public"))
 
 app.get("/", function(req, res) {
-    res.send("Hello world  express")
+    res.send("Hello world express")
 })
+
 var spotifyApi = new SpotifyWebApi({
     clientId: "6c147d1edb16457cbce816d434b50dd3",
     clientSecret: "6a0dbcf0ba864168b228689c2af0fcdc"
@@ -16,7 +18,6 @@ spotifyApi.clientCredentialsGrant().then(
     function (data) {
         console.log("The access token expres in " + data.body["expires_in"]);
         console.log("The access token is " + data.body["access_token"]);
-
         
         // Save the access token so that it's used in future calls
         spotifyApi.setAccessToken(data.body["access_token"]);
@@ -36,33 +37,35 @@ async function getTracks(searchterm, res) {
     }, function (err) {
         console.error(err);
     });
+
+    spotifyApi.searchTracks(searchterm).then(function (data) {
+        var tracks = data.body.tracks.items
+        var HTMLResponce = "";
+
+        for(var i=0; i<tracks.length; i++){
+            var track = tracks[i];
+            console.log(track.name);
+
+            HTMLResponse += `
+                    <div>
+                        <h2>${track.name}</h2>
+                        <h4>${track.artists[0].name}</h4>
+                        <img src='${track.album.images[0].url}'>
+                        <a href='${track.external_urls.spotify}'>Track Details</a>
+                    </div>`;
+            console.log(HTMLResponce);
+        }
+        res.send(HTMLResponce)
+    }, function (err) {
+        console.error(err);
+    });
 }
+
 
 // Route for love in tracks, artists and albums
 app.get("/searchlove", function (req, res) {
     getTracks("love", res);
 });
 
-spotifyApi.searchTracks(searchterm).then(function (data) {
-    var tracks = data.body.tracks.items
-    var HTMLResponce = "";
-
-    for(var i=0; i<tracks.length; i++){
-        var track = tracks[i];
-        console.log(track.name);
-
-        HTMLResponce = HTMLResponce + 
-        `<div> +
-            <h2>+track.name+<h2>+
-            <h4>+tracks.artists[0].name+<h4>+
-            <img src='+track.album.images[0].url +'>+
-            <a href='+track.external_urls.spotify+'> Track Details </a>+
-        </div>`;
-        console.log(HTMLResponce);
-    }
-    res.send(HTMLResponce)
-}, function (err) {
-    console.error(err);
-});
 
 app.listen(8080)
